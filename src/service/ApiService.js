a53b067a6689befa86cb7ca833650f6cce15c8e8
Muhammad Ali <122118041+AleeDe@users.noreply.github.com>
2025-06-01 20:36:47 +0500

    Update ApiService.js

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export default class ApiService {
   
-    static BASE_URL = process.env.REACT_APP_API_URL;
+    const BASE_URL = process.env.REACT_APP_API_URL;
 	// console.log(BASE_URL);
     static getHeaders() {
         const token = localStorage.getItem("token");
